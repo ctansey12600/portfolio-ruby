76d43d76bb566ae6d226ad4e9e1f29a88e7c30c3
Casey Tansey <ctansey12600@outlook.com>
2022-10-07 19:59:24 -0400

    question route added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,5 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Returns all portfolios
-  get "/portfolios" do
-    portfolio = Portfolio.all
-    portfolio.to_json(include: {projects: {include: [:photos, :links]}})
-  end
-
-  # # Returns portfolios by id
-  # get "/portfolios/:id" do
-  #   portfolio = Portfolio.find(params[:id])
-  #   portfolio.to_json(include: {projects: {include: [:photos, :links]}})
-  # end
 
   # Returns only website portfolio data
   get "/portfolios/website" do
@@ -29,6 +17,11 @@ class ApplicationController < Sinatra::Base
   get "/portfolios/blog" do
     portfolio = Portfolio.where(name: "Blog")
     portfolio.to_json(include: {projects: {include: [:photos, :links]}})
+  end
+
+  get "/questions" do
+    question = Question.all
+    question.to_json(include: :user)
   end
 
 end
