0e1f7bb77d779cda411b9713f685d3de6981e56e
Casey Tansey <ctansey12600@outlook.com>
2022-10-07 17:58:24 -0400

    Routes added in Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,31 @@ class ApplicationController < Sinatra::Base
   # Returns all portfolios
   get "/portfolios" do
     portfolio = Portfolio.all
-    portfolio.to_json
+    portfolio.to_json(include: {projects: {include: [:photos, :links]}})
   end
 
-  get "/portfolios/id" do
+  # Returns portfolios by id
+  get "/portfolios/:id" do
     portfolio = Portfolio.find(params[:id])
-    portfolio.to_json
+    portfolio.to_json(include: {projects: {include: [:photos, :links]}})
   end
+
+  # Returns only website portfolio data
+  get "/portfolios/website" do
+    portfolio = Portfolio.where(name: "Website Development")
+    portfolio.to_json(include: {projects: {include: [:photos, :links]}})
+  end
+
+  # Returns only graphic portfolio data
+  get "/portfolios/graphic" do
+    portfolio = Portfolio.where(name: "Graphic Design")
+    portfolio.to_json(include: {projects: {include: [:photos, :links]}})
+  end
+
+  # Returns only blog portfolio data
+  get "/portfolios/blog" do
+    portfolio = Portfolio.where(name: "Blog")
+    portfolio.to_json(include: {projects: {include: [:photos, :links]}})
+  end
+
 end
