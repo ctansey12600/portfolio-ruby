e86a8983839ce8e4253bdd84fec9adfc7d3084fb
Casey Tansey <ctansey12600@outlook.com>
2022-10-10 18:41:07 -0400

    response for questions added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,29 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+  # Returns by name match rather than id to ensure that loaded info is the same regardless if id where to change
   # Returns only website portfolio data
   get "/portfolios/website" do
-    portfolio = Portfolio.where(name: "Website Development")
+    portfolio = Portfolio.find_by(name: 'Website Development')
     portfolio.to_json(include: {projects: {include: [:photos, :links]}})
   end
 
   # Returns only graphic portfolio data
   get "/portfolios/graphic" do
-    portfolio = Portfolio.where(name: "Graphic Design")
+    portfolio = Portfolio.find_by(name: 'Graphic Design')
     portfolio.to_json(include: {projects: {include: [:photos, :links]}})
   end
 
   # Returns only blog portfolio data
   get "/portfolios/blog" do
-    portfolio = Portfolio.where(name: "Blog")
+    portfolio = Portfolio.find_by(name: 'Blog')
     portfolio.to_json(include: {projects: {include: [:photos, :links]}})
+  end
+
+  # Returns all questions
+  get "/questions" do
+    question = Question.all
+    question.to_json(include: :user)
   end
 
   post "/questions" do
