f60013c3f1130e4691b859d40d2ee80f23666688
Casey Tansey <ctansey12600@outlook.com>
2022-10-11 10:12:45 -0400

    Column added to Questions Table for Answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_172316) do
+ActiveRecord::Schema.define(version: 2022_10_11_141026) do
 
   create_table "links", force: :cascade do |t|
     t.string "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_10_07_172316) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "answer"
   end
 
   create_table "users", force: :cascade do |t|
